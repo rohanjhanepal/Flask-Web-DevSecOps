d56011e487294f245279336de1e47d9658c08d6b
Rohan Jha <rohanjhanepal@gmail.com>
2025-05-18 00:10:54 +1000

    added

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,17 @@
 import pytest
-# from main import create_app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from website import create_app
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 def test_home_page(client):
-    assert True
-    # response = client.get('/')
-    # assert response.status_code == 200
-    # assert b"Home Page" in response.data  
+    response = client.get('/')
+    assert response.status_code == 302
 
-# def test_login_page(client):
-#     response = client.get('/login')
-#     assert response.status_code == 200
-#     assert b"Login" in response.data
